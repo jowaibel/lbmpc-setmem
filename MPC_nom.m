595d1bf6bd632365545de5406d34dd9b5c20a097
Aerlio <44504505+Aerlio@users.noreply.github.com>
2021-06-11 16:39:07 -0400

    generalized for different statemodels

--- MPC_nom.m
+++ MPC_nom.m
@@ -1,7 +1,4 @@
-function [U,X,u] = MPC_nom(Ac,Bc,x_ini,time_res,alpha_ref,gamma_ref,H)
-    % Discretized system
-    A = eye(4) + time_res*Ac;
-    B = time_res*Bc;
+function [U,X,u] = MPC_nom(A,B,x_ini,ref,s,H)
     
     sx=4; %num. states
     su=1; %num. inputs
@@ -15,12 +12,13 @@ function [U,X,u] = MPC_nom(Ac,Bc,x_ini,time_res,alpha_ref,gamma_ref,H)
     constraints_MPC=[x_i(:,1)==x_0];
     for i=1:H
         % Stage cost
-        objective_MPC=objective_MPC   +   (x_i(2,i+1)-alpha_ref(1,i))^2   +    (x_i(4,i+1)-gamma_ref(1,i))^2;
+        for it_s = 1:size(s,1)
+            objective_MPC=objective_MPC   +   (x_i(s(it_s),i)-ref(it_s,i))^2;
+        end
         % State Propagation Constraints
         constraints_MPC=[constraints_MPC, x_i(:,i+1) == A*x_i(:,i)+B*u_i(:,i)];
         
         % State & Input Constraints
-        constraints_MPC=[constraints_MPC, x_i(1,i+1) >= 0]; %Va (velocity norm) major to zero always
         constraints_MPC=[constraints_MPC, deg2rad(-30) <= u_i(1,i) <= deg2rad(30)]; % Elevator deflection is limited by +-30 deg
     end
     ops = sdpsettings('verbose',1,'solver','sedumi');
